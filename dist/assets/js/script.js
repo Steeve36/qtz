1c226b4a4e70929a0b1af5b86a9cf0821bedf2c6
Steeve MOKOKO <steeve.mokoko14@gmail.com>
2025-04-06 04:26:16 +0200

    NavBar responsive avec sidebar

---
+++ dist/assets/js/script.js
@@ -0,0 +1,11 @@
+// ===================
+// CODE DE LA SIDEBAR - OUVRIR ET FERMER LA SIDEBAR
+// ===================
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
